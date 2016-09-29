7f74cb4eed3ef809e6dc3c9281d3d7c09595cc5c
M4thG33k <pepethemathgeek@gmail.com>
2016-09-29 06:55:21 -0500

    Bare-bones initial commit

---
+++ src/main/java/com/m4thg33k/pipedream/PipeDream.java
@@ -0,0 +1,47 @@
+package com.m4thg33k.pipedream;
+
+import com.m4thg33k.pipedream.core.proxy.CommonProxy;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+
+@Mod(modid = PipeDream.MODID, name = PipeDream.MODNAME, version = PipeDream.VERSION)
+public class PipeDream {
+
+    public static final String MODID = "pipedream";
+    public static final String VERSION = "@VERSION@";
+    public static final String MODNAME = "PipeDream";
+
+    @Mod.Instance
+    public static PipeDream INSTANCE = new PipeDream();
+
+    @SidedProxy(clientSide = "com.m4thg33k.pipedream.core.proxy.ClientProxy", serverSide = "com.m4thg33k.pipedream.core.proxy.ServerProxy")
+    public static CommonProxy proxy;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        proxy.preinit(event);
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        proxy.init(event);
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        proxy.postinit(event);
+    }
+
+    @Mod.EventHandler
+    public void serverLoad(FMLServerStartingEvent event)
+    {
+        //Initialize commands here
+    }
+}
